6420468e4d2aaa18e5f4d9401c6953f68a6b9a33
Zaouga Eya <eya.zaouga@gmail.com>
2024-02-27 19:38:34 +0200

    ajout commentaire

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -20,7 +20,7 @@ import org.json.JSONObject;
  */
 
 public class CallRestWebService {
-
+/**hello eya et farah first projet */
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
